ce860ce1e4d8405a04b5b6e86948562b1ed8af65
Armando <piruk7@gmail.com>
2012-06-13 11:48:19 -0300

    Start Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gvar::Application.config.secret_token = '847e49b7abfad9c618b2ddeb448abc8c03f6a3b8dc45777734c4000b7bbdfed2e39a75f3b1dbc6c6876993cbef76494b74baa540da99f7ff3bfb52b75a48289f'
