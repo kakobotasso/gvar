db76f73c4439542595108efb864985258af534f4
Armando Piruk <piruk7@gmail.com>
2012-09-01 06:53:25 +0200

    Adicionado relacionamento entre Release x Instalment

--- app/models/instalment.rb
+++ app/models/instalment.rb
@@ -1,6 +1,6 @@
 class Instalment < ActiveRecord::Base
   attr_accessible :expiration_date, :name, :number, :payment, :price, :release_id, :status
 
-belongs_to :release
+  belongs_to :release
 
 end
--- app/models/release.rb
+++ app/models/release.rb
@@ -1,5 +1,5 @@
 class Release < ActiveRecord::Base
-  attr_accessible :category_id, :code, :description, :first_paid, :fixed_payment, :name, :number_instalments, :total_price
+  attr_accessible :category_id, :code, :description, :first_paid, :fixed_payment, :name, :number_instalments, :total_price, :instalments_attributes
 
   has_many :instalments
 
