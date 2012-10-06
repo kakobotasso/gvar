c79e430d8fbc953c8fbe249695a7e650917b9018
Armando <piruk7@gmail.com>
2012-10-05 23:23:03 -0300

    Adicionado seed para gerar Lancamentos e Parcelas aleatorias

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,27 @@ User.create!(
   :email => "user@teste.com",
   :role_ids => [2,4,6]
 )
+
+code = 1210021000
+(1..50).each do |n|
+  temp = (rand(10)+1)
+  r = Release.new do |r|
+    r.code = "P#{code}"
+    r.name = "Lancamento #{n}"
+    r.category_id = rand(10)
+    r.description = "Descricao #{n}"
+  end
+  (1..temp).each do |parcela|
+    r.instalments << Instalment.create! do |i|
+      i.number = parcela
+      i.expiration_date = Date.today + (parcela * 10)
+      i.amount = temp * 100
+      i.status_id = 0
+      i.payment_id = 0
+    end
+  end
+  r.total_amount = r.instalments.collect{|parcela| parcela.amount}.inject(:+)
+  r.number_instalments = r.instalments.size
+  r.save
+  code += 1
+end
