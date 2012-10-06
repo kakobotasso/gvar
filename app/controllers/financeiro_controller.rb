19fd1bc7b9376f3c33e301447e5ad37feb9522f3
Armando <piruk7@gmail.com>
2012-10-05 23:26:51 -0300

    Alterado controller para trabalhar com Filter e Presenter

--- app/controllers/financeiro_controller.rb
+++ app/controllers/financeiro_controller.rb
@@ -3,7 +3,11 @@ class FinanceiroController < ApplicationController
   require_role_for_pagamento
 
   def index
-    @release = Release.all
+    @list_instalments = InstalmentsFilter.filter(params['filtro'])
+    @instalment = InstalmentPresenter.new(@list_instalments)
+
+    # Exibe as 4 parcelas mais proximas de vencer
+    @instalments_recents = Instalment.order("expiration_date").limit(4)
     @categories = Category::OPTIONS
   end
 
@@ -11,14 +15,20 @@ class FinanceiroController < ApplicationController
   	@release  = Release.new
   	@release.instalments.build
 
-        # release_code tera o formato:
-        # P120927205100
-        # [P|R] + ano + mes + dia + hora + min + seg
-        @release_code = "P#{Time.now.strftime('%y%m%d%H%M%S')}"
+    # release_code tera o formato:
+    # P120927205100
+    # [P|R] + ano + mes + dia + hora + min + seg
+    @release_code = "P#{Time.now.strftime('%y%m%d%H%M%S')}"
 
   	@status     = Status::FINANCIAL
   	@payments   = Payment::OPTIONS
   	@categories = Category::OPTIONS
+  end
+
+  def consultar
+  end
+
+  def relatorio
   end
 
 end
