2fc9f380f2fcb8609be584c8c4970c594115f4c7
Armando <piruk7@gmail.com>
2012-10-21 23:58:22 -0300

    Adicionada actions para possibilitar o editar/pagar

--- app/controllers/financeiro_controller.rb
+++ app/controllers/financeiro_controller.rb
@@ -12,17 +12,55 @@ class FinanceiroController < ApplicationController
   end
 
   def novo_pagamento
-  	@release  = Release.new
-  	@release.instalments.build
+    @release  = Release.new
+    @release.instalments.build
 
     # release_code tera o formato:
     # P120927205100
     # [P|R] + ano + mes + dia + hora + min + seg
     @release_code = "P#{Time.now.strftime('%y%m%d%H%M%S')}"
 
-  	@status     = Status::FINANCIAL
-  	@payments   = Payment::OPTIONS
-  	@categories = Category::OPTIONS
+    @status     = Status::FINANCIAL
+    @payments   = Payment::OPTIONS
+    @categories = Category::OPTIONS
+  end
+
+  def pagar
+    @instalment = Instalment.find(params[:id])
+
+    @payments   = Payment::OPTIONS
+  end
+
+  def pagar_parcela
+    @instalment = Instalment.find(params[:pay_instalment][:id])
+
+    # Se a forma de pagamento foi :cheque ou :cheque-pre
+    # deixa com status de :recebido
+    # caso contrario o status fica como :quitado
+    if [1,2].include? params[:pay_instalment][:payment_id].to_i
+      params[:pay_instalment][:status_id] = 3
+    else
+      params[:pay_instalment][:status_id] = 1
+    end
+    @instalment.update_attributes(params[:pay_instalment])
+
+    redirect_to financeiro_index_path
+  end
+
+  def editar_lancamento
+    @release = Release.find(params[:id])
+    @instalments = @release.instalments
+
+    @status     = Status::FINANCIAL
+    @payments   = Payment::OPTIONS
+    @categories = Category::OPTIONS
+  end
+
+  def update_lancamento
+    @release = Release.find(params[:release][:id])
+    @release.update_attributes(params[:release])
+
+    redirect_to financeiro_index_path    
   end
 
   def consultar
