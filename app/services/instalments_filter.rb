6bf1cebde0d2a90f059ecd3895568a199c197e0a
Armando <piruk7@gmail.com>
2012-10-22 01:44:24 -0300

    Update no filtro de parcelas para nao trazer as quitadas e filtrar o codigo da pesquisa simples

--- app/services/instalments_filter.rb
+++ app/services/instalments_filter.rb
@@ -6,12 +6,13 @@ class InstalmentsFilter
     scope = scope.where(:number => options['number']) unless options['number'].blank?
     scope = scope.where(:'releases.name' => options['name']) unless options['name'].blank?
     scope = scope.where(:'releases.code' => options['code']) unless options['code'].blank?
-    scope = scope.where(:'releases.code' => options['code']) unless options['code_simple'].blank?
+    scope = scope.where(:'releases.code' => options['code_simple']) unless options['code_simple'].blank?
     scope = scope.where("amount >= :min", {:min => options['amount_minimun']}) unless options['amount_minimun'].blank?
     scope = scope.where("amount <= :max", {:max => options['amount_max']}) unless options['amount_max'].blank?
     # implementar depois o fixed_payment
     # scope = scope.where(:'releases.fixed_payment' => options['fixed_payment']) unless options['fixed_payment'].blank?
     scope = scope.where(:'releases.category_id' => options['category_id']) unless options['category_id'].blank?
+    scope = scope.where("status_id != 1")
     scope
     else
       [] # Ou então retorna um array vazio para a view dizer que nao encontrou resultados
