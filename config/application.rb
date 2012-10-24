b0eb81a545ea60fd9616ffe81fbda4778639a8de
Armando <piruk7@gmail.com>
2012-10-24 00:05:50 -0300

    Alterado padrão de time_zone e load_path para o i18n

--- config/application.rb
+++ config/application.rb
@@ -33,10 +33,8 @@ module Gvar
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.time_zone = 'Brasilia'
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
     config.i18n.default_locale = :"pt-BR"
 
     # Configure the default encoding used in templates for Ruby 1.9.
