32b8090b14ea42d624736492b1d4d7cb07a7e1ec
Kako Botasso <kakodev@gmail.com>
2012-11-14 01:30:22 -0200

    Ajustes interessados

--- app/models/interested.rb
+++ app/models/interested.rb
@@ -2,6 +2,7 @@ class Interested < ActiveRecord::Base
   attr_accessible :email, :interest, :name, :phone, :situation, :interested_contacts_attributes
   has_many :interested_contacts
   accepts_nested_attributes_for :interested_contacts
+  validates_presence_of :email, :interest, :name, :phone, :situation
 
   def real_situation
   	Category::Interested::SITUATION.rassoc(situation)[0]
