5e0b1ec18a292fd02bf90ebb4d7ce8e2e3802b7d
Armando <piruk7@gmail.com>
2012-11-16 20:00:31 -0300

    Metodo para visualizar se servico esta ativou ou nao

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,9 @@
+# -*- encoding:utf-8 -*-
 class Service < ActiveRecord::Base
   attr_accessible :default_value, :description, :estimated_time, :inactive, :name, :number_days
   has_and_belongs_to_many :providers
+
+  def active
+    self.inactive? ? "Não" : "Sim"
+  end
 end
