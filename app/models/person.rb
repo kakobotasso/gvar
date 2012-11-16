16cc87252b4dcc40dd72a0f847206a985162a7fe
Armando <piruk7@gmail.com>
2012-11-16 19:48:19 -0300

    Criada model pai para Client e Provider

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
-  attr_accessible :address_id, :cpf, :name, :rg, :observations
+  attr_accessible :address_attributes, :cpf, :name, :rg, :observations, :service_ids, :active
   has_one :address
+  accepts_nested_attributes_for :address
 end
