eb065d88509220a12d11e3170db02e050863b8d1
Armando <piruk7@gmail.com>
2012-11-16 19:48:59 -0300

    Model address para Person

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ActiveRecord::Base
-  attr_accessible :address, :city, :complement, :country, :neighborhood, :number, :zipcode
+  attr_accessible :address, :city, :complement, :country_id, :neighborhood, :number, :zipcode
   belongs_to :person
 end
