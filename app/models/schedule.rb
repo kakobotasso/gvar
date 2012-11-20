b37e77fffd34e969354e69c04e0b2f164fb17bd5
Armando <piruk7@gmail.com>
2012-11-20 18:47:30 -0300

    Crud para Schedule

---
+++ app/models/schedule.rb
@@ -0,0 +1,7 @@
+class Schedule < ActiveRecord::Base
+  attr_accessible :client_id, :code, :date, :estimated_time, :number_days, :provider_id, :service_id
+
+  belongs_to :client
+  belongs_to :provider
+  belongs_to :service
+end
---
+++ db/migrate/20121120155656_create_schedules.rb
@@ -0,0 +1,16 @@
+class CreateSchedules < ActiveRecord::Migration
+  def change
+    create_table :schedules do |t|
+      t.string   :code
+      t.integer  :service_id
+      t.integer  :provider_id
+      t.integer  :client_id
+      t.string   :estimated_time
+      t.integer  :number_days
+      t.datetime :date
+      t.text     :observations
+
+      t.timestamps
+    end
+  end
+end
