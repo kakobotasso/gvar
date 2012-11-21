0f135a1440ea6bd2f2835bcc1c1306c948949a96
Kako Botasso <kakodev@gmail.com>
2012-11-21 00:57:19 -0200

    Ajustes menus modulos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def menu
+		case self.controller_name.to_sym
+			when :cursos, :turmas, :alunos, :interessados, :inscricoes then render '/shared/menu_courses.html.erb'
+			when :clients, :providers, :services, :schedules then render '/shared/menu_services.html.erb'
+			when :payments, :receipts then render '/shared/menu_financial.html.erb'
+			when :users then render '/shared/menu_users.html.erb'
+		end
+	end
 end
