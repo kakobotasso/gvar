fa363130b69558928289b5f22cc656afa414fdb8
Armando <piruk7@gmail.com>
2012-12-04 01:05:45 -0200

    Objeto para abstrair entidade que sera exibida em Receipt#new

--- app/services/receipt_factory.rb
+++ app/services/receipt_factory.rb
@@ -2,24 +2,24 @@ class ReceiptFactory
   @struct = OpenStruct.new
 
   def self.new_receipt(object)
-    receipt_from_schedule(object)     if object.instance_of? Schedule
-    receipt_from_registration(object) if object.instance_of? Registration
+    return receipt_from_schedule(object)     if object.instance_of? Schedule
+    return receipt_from_registration(object) if object.instance_of? Registration
   end
 
   def self.receipt_from_schedule(object)
     @struct.code        = object.code
     @struct.name        = object.client.name
     @struct.cpf         = object.client.cpf
-    @struct.description = object.description
+    @struct.description = object.observations
 
     @struct
   end
 
   def self.receipt_from_registration(object)
     @struct.code        = object.code
-    @struct.name        = object.client.name
-    @struct.cpf         = object.client.cpf
-    @struct.description = object.description
+    @struct.name        = object.student.name
+    @struct.cpf         = object.student.cpf
+    @struct.description = object.team.note
 
     @struct
   end
