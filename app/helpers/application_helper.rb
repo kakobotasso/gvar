67b11c987673092d7ed384072c8f1d8c3cc536ed
Armando <piruk7@gmail.com>
2012-12-04 01:02:00 -0200

    Adicionado metodo helper para estilizar datas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-	def menu
-		case self.controller_name.to_sym
-			when :courses, :teams, :students, :interesteds, :registrations then render '/shared/menu_courses'
-			when :clients, :providers, :services, :schedules then render '/shared/menu_services'
-			when :payments, :receipts then render '/shared/menu_financial'
-			when :users then render '/shared/menu_users'
-		end
-	end
+  def menu
+    case self.controller_name.to_sym
+    when :courses, :teams, :students, :interesteds, :registrations then render '/shared/menu_courses'
+    when :clients, :providers, :services, :schedules then render '/shared/menu_services'
+    when :payments, :receipts then render '/shared/menu_financial'
+    when :users then render '/shared/menu_users'
+    end
+  end
+
+  def date_to_view(date)
+    date.strftime("%d/%m/%Y")
+  end
 end
