1f311ba55f37f9dff998a6d7d4bf8e548750efa4
Kako Botasso <kakodev@gmail.com>
2012-12-09 13:52:13 -0200

    Passando as mascaras para um unico lugar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,6 +61,26 @@ $(function(){
         return false;
     });
     /* / REALIZA A PESQUISA */
+
+    /* MASCARAS */
+    $(".cpf").mask("999.999.999-99");
+    $(".rg").mask("99.999.999-*");
+    $(".cep").mask("99999-999");
+    $(".data").mask("99/99/9999");
+    // 9 DIGITO OPCIONAL
+    $('.celular').mask("(99) 9999-9999?9").live('focusout', function (event) {
+        var target, phone, element;
+        target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+        phone = target.value.replace(/\D/g, '');
+        element = $(target);
+        element.unmask();
+        if(phone.length > 10) {
+            element.mask("(99) 99999-999?9");
+        } else {
+            element.mask("(99) 9999-9999?9");
+        }
+    });
+    /* / MASCARAS */
 });
 
 /* PLUGIN resetDefaultValue */
