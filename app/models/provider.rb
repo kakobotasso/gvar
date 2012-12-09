b3335d76e51fd01369d9c27f540df7b2c88388d2
Kako Botasso <kakodev@gmail.com>
2012-12-09 18:39:00 -0200

    Ajustes em prestadores de servicos

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -4,7 +4,14 @@ class Provider < Person
   has_and_belongs_to_many :services
   has_many :schedules
 
+  validates_presence_of :name, :cpf, :rg
+
   def is_active
     self.active? ? "Sim" : "Não"
   end
+
+  def estado
+  	Country::STATES.rassoc(address.country_id)[0]
+  end
+
 end
