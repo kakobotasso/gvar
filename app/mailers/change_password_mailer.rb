49a41d9edac389fdaf6df28639dd5e25ec5976db
Armando <piruk7@gmail.com>
2013-02-26 02:36:04 -0300

    Erro no layout de email

--- app/mailers/change_password_mailer.rb
+++ app/mailers/change_password_mailer.rb
@@ -4,7 +4,7 @@ class ChangePasswordMailer < ActionMailer::Base
   def new_password_mail(options)
     @usuario = options["name"]
     @senha   = options["password"]
-    @url     = root_url
+    @url     = "http://192.34.58.203"
     @subject = options["subject"] || "Nova senha para acesso ao GVAR"
 
     mail(:to => options["email"], :subject => @subject)
